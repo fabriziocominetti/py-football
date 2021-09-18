f290bcf84dd8f151ea46b799e7cb4001c449f8ff
Fabrizio Cominetti <75806093+fabriziocominetti@users.noreply.github.com>
2021-09-18 15:08:24 +0200

    Update code.py

--- Football-4Teams-League Simulator/code.py
+++ Football-4Teams-League Simulator/code.py
@@ -5,65 +5,73 @@ team2 = (input("Please enter the second team: "))
 team3 = (input("Please enter the third team: "))
 team4 = (input("Please enter the fourth team: "))
 for i in range(5):
-    score1 = randint(0,5)
+    score1_1 = randint(0,5)
+    score1_2 = randint(0,5)
+    score1_3 = randint(0,5)
 for i in range(5):
-    score2 = randint(0,5)
+    score2_1 = randint(0,5)
+    score2_2 = randint(0,5)
+    score2_3 = randint(0,5)
 for i in range(5):
-    score3 = randint(0,5)
+    score3_1 = randint(0,5)
+    score3_2 = randint(0,5)
+    score3_3 = randint(0,5)
 for i in range(5):
-    score4 = randint(0,5)
+    score4_1 = randint(0,5)
+    score4_2 = randint(0,5)
+    score4_3 = randint(0,5)
 points1 = 0
 points2 = 0
 points3 = 0
 points4 = 0
 #matches
 print("1st round")
-print("Final Score: "+team1.upper()+" "+str(score1)+" : "+team2.upper()+" "+str(score2))
-if str(score1) > str(score2):
+print("Final Score: "+team1.upper()+" "+str(score1_1)+" : "+team2.upper()+" "+str(score2_1))
+if str(score1_1) > str(score2_1):
     points1 += 3
-elif str(score1) == str(score2):
+elif str(score1_1) == str(score2_1):
     points1 += 1
     points2 += 1
 else:
     points2 += 3
-print("Final Score: "+team3.upper()+" "+str(score3)+" : "+team4.upper()+" "+str(score4))
-if str(score3) > str(score4):
+print("Final Score: "+team3.upper()+" "+str(score3_1)+" : "+team4.upper()+" "+str(score4_1))
+if str(score3_1) > str(score4_1):
     points3 += 3
-elif str(score3) == str(score4):
+elif str(score3_1) == str(score4_1):
     points3 += 1
     points4 += 1
 else:
     points4 += 3
 print("2nd round")
-print("Final Score: "+team1.upper()+" "+str(score1)+" : "+team3.upper()+" "+str(score3))
-if str(score1) > str(score3):
+print("Final Score: "+team1.upper()+" "+str(score1_2)+" : "+team3.upper()+" "+str(score3_2))
+if str(score1_2) > str(score3_2):
     points1 += 3
-elif str(score1) == str(score3):
+elif str(score1_2) == str(score3_2):
     points1 += 1
     points3 += 1
 else:
     points3 += 3
-print("Final Score: "+team4.upper()+" "+str(score4)+" : "+team2.upper()+" "+str(score2))
-if str(score4) > str(score2):
+print("Final Score: "+team4.upper()+" "+str(score4_2)+" : "+team2.upper()+" "+str(score2_2))
+if str(score4_2) > str(score2_2):
     points4 += 3
-elif str(score4) == str(score2):
+elif str(score4_2) == str(score2_2):
     points4 += 1
     points2 += 1
 else:
     points2 += 3
 print("3rd round")
-print("Final Score: "+team1.upper()+" "+str(score1)+" : "+team4.upper()+" "+str(score4))
-if str(score1) > str(score4):
+print("Final Score: "+team1.upper()+" "+str(score1_3)+" : "+team4.upper()+" "+str(score4_3))
+if str(score1_3) > str(score4_3):
     points1 += 3
-elif str(score1) == str(score4):
+elif str(score1_3) == str(score4_3):
     points1 += 1
     points4 += 1
 else:
     points4 += 3
-print("Final Score: "+team2.upper()+" "+str(score2)+" : "+team3.upper()+" "+str(score3))
-if str(score2) > str(score3):
+print("Final Score: "+team2.upper()+" "+str(score2_3)+" : "+team3.upper()+" "+str(score3_3))
+if str(score2_3) > str(score3_3):
     points2 += 3
-elif str(score2) == str(score3):
+elif str(score2_3) == str(score3_3):
     points2 += 1
     points3 += 1
 else:
